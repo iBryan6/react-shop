34de0fd1e79ad5ccd64626261999dfb442680f52
Laboratorio5Pc4 <Laboratorio5Pc4@UCATEC.local>
2018-09-07 07:52:36 -0400

    test

--- src/backend/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "accesscell";
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName) or die("Error al conectar a la Base de Datos, revisar Servidor");
-?>
