b3dd767b255eb8d57b7d55979129b811578e2d4e
Laboratorio5Pc4 <Laboratorio5Pc4@UCATEC.local>
2018-09-07 07:51:31 -0400

    idea

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 json-server --watch catalog.json --port 3004
+
+https://hashnode.com/post/how-can-use-react-js-node-js-mysql-together-cjdlfbukh01vqn9wuaucmng6h
---
+++ src/backend/connect.php
@@ -0,0 +1,7 @@
+<?php
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "accesscell";
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName) or die("Error al conectar a la Base de Datos, revisar Servidor");
+?>
